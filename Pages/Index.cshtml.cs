9d42050081657bcaf6fbf8dcda458f5d3c591c02
chenxizhang <ares@xizhang.com>
2022-04-21 13:03:08 +0800

    初始化

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Graph;
+using Microsoft.Identity.Web;
+
+namespace graphweb.Pages;
+
+[AuthorizeForScopes(ScopeKeySection = "DownstreamApi:Scopes")]
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    private readonly IDownstreamWebApi _downstreamWebApi;
+    private readonly GraphServiceClient _client;
+
+    public IndexModel(ILogger<IndexModel> logger,
+                        GraphServiceClient client,
+                        IDownstreamWebApi downstreamWebApi)
+    {
+        _logger = logger;
+        _downstreamWebApi = downstreamWebApi;
+        _client = client;
+
+    }
+
+    public async Task OnGet()
+    {
+        var me = await _client.Me.Request().GetAsync();
+        ViewData["me"] = $"显示名称：{me.DisplayName},邮件地址：{me.UserPrincipalName}";
+
+        var messages = await _client.Me.Messages.Request().GetAsync();
+        ViewData["messages"] = messages.Select(x => x.Subject).ToArray();
+    }
+}
